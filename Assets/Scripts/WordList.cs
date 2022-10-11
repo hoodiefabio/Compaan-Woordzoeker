7e640c619d909ba3b13a9d6dfa22398d88c226c9
hoodiefabio <73169440+hoodiefabio@users.noreply.github.com>
2022-10-11 10:57:17 +0200

    Randomly chosen words

--- Assets/Scripts/WordList.cs
+++ Assets/Scripts/WordList.cs
@@ -1,35 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class WordList : MonoBehaviour
 {
     public List<string> words;
+    public int amountOfWords = 3;
     List<string> pickedWords;
     [SerializeField] Text displayText;
+    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        foreach (string word in words)
-        {
-            displayText.text = displayText.text + "\n" + word;
-        }
+        PickRandomWords(amountOfWords);
     }
     
 
-    // Update is called once per frame
-    void Update()
+    void PickRandomWords(int amount)
     {
 
+        for (int i = 0; i < words.Count; i++)
+        {
+            string temp = words[i];
+            int randomIndex = Random.Range(i, words.Count);
+            words[i] = words[randomIndex];
+            words[randomIndex] = temp;
+        }
+
+        for (int i = 0; i < amount; i++)
+        {
+            displayText.text = displayText.text + "\n" + words[i];
+        }
     }
 
-    void PickRandomWords()
-    {
-       // pickedWords.Add(words[Random.Range(0, words.Count)]);
-       
-    }
+
+
+
 }
